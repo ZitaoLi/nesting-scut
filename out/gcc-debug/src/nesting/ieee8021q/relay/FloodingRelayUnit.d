52e1b8f26bc90c6fe34e4cd6c8ad85068743a468
ZitaoLi <494240799@qq.com>
2019-11-07 04:50:00 -0800

    update .gitignore

--- out/gcc-debug/src/nesting/ieee8021q/relay/FloodingRelayUnit.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/nesting/ieee8021q/relay/FloodingRelayUnit.o: \
- nesting/ieee8021q/relay/FloodingRelayUnit.cc \
- nesting/ieee8021q/relay/FloodingRelayUnit.h \
- /home/lztao/Downloads/omnetpp-5.4.1/include/omnetpp.h
-
-nesting/ieee8021q/relay/FloodingRelayUnit.h:
-
-/home/lztao/Downloads/omnetpp-5.4.1/include/omnetpp.h:
--- out/gcc-debug/src/nesting/linklayer/framePreemption/FrameForward.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/nesting/linklayer/framePreemption/FrameForward.o: \
- nesting/linklayer/framePreemption/FrameForward.cc \
- nesting/linklayer/framePreemption/FrameForward.h \
- /home/lztao/Downloads/omnetpp-5.4.1/include/omnetpp.h
-
-nesting/linklayer/framePreemption/FrameForward.h:
-
-/home/lztao/Downloads/omnetpp-5.4.1/include/omnetpp.h:
