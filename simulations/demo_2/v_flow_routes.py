f36159842f30be4f85ba17df26fdbb2a97ccaefd
ZitaoLi <494240799@qq.com>
2020-01-10 01:15:39 +0800

    add test cases

--- simulations/demo_2/v_flow_routes.py
+++ simulations/demo_2/v_flow_routes.py
@@ -8,14 +8,19 @@ imgs = []
 for filename in filenames:
     if os.path.splitext(filename)[1] == '.png':
         img = Image.open(os.path.join(DIR_NAME, filename))
-        # img.show()
         imgs.append(img)
 width, height = imgs[0].size
-left = 0
-right = width
-result = Image.new(imgs[0].mode, (width * len(imgs), height))
-for i, img in enumerate(imgs):
-    result.paste(img, (left, 0, right, height))
-    left += width
-    right += width
+result = Image.new(imgs[0].mode, (width * 2, height * 2))
+
+img1 = Image.new('RGB', (int(width / 2), height), (255, 255, 255))
+img2 = Image.new('RGB', (int(width / 2), height), (255, 255, 255))
+imgs.insert(2, img1)
+imgs.insert(4, img2)
+
+result.paste(imgs[0], (0, 0, width, height))
+result.paste(imgs[1], (width, 0, width + width, height))
+result.paste(imgs[2], (0, height, int(width / 2), height * 2))
+result.paste(imgs[3], (int(width / 2), height, width + int(width / 2), height * 2))
+result.paste(imgs[4], (int(width + width / 2), height, width * 2, height * 2))
+
 result.show()
